bfb291bcc89fd3ff80b9949712f392f0f3010ae5
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-20 00:48:07 +1000

    small changes/ need to fix to pick up search correctly

--- script.js
+++ script.js
@@ -1,32 +1,19 @@
   //variables
-    var citySearch = $("#city").val();
-    var queryURL = "api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=b97ce200929c2749eca4924f16dc7e98";
+    var citySearch = $("#citySearched").val()
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=b97ce200929c2749eca4924f16dc7e98";
 
+//Start process when search button clicked
+  $("#search").on("click", function(event) {
+    event.preventDefault();
 
-  //ajax call
+    console.log(citySearch)
+    console.log(queryURL)
+
+//ajax call
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
+  }).then(function(response) {
     console.log(response);
   });
-
-
-  //Save searches in local storage
-  function searchTemp(){
-    localStorage.setItem(Searched,"citySearch");
-  }
-  
-
-
-
- 
-function formatSearch(jsonObject) {
-    var cityName = jsonObject.name;
-    var cityWeather = jsonObject.weather[0].main;
-    var cityTemp = jsonObject.main.temp;
-
-    $("city-name").text(cityName);
-    $("city-name").text(cityWeather);
-    $("city-name").text(cityTemp);
-} 
+});
