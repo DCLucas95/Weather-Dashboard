650597680c07a758f819b6db9047239169312024
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-20 20:45:16 +1000

    fixed temperature showing Kelvin. Searches now shown and read from local storage

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ $("#search").on("click", function (event) {
   var citySearch = $("#citySearched").val()
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=b97ce200929c2749eca4924f16dc7e98";
   event.preventDefault();
-
+  
   //ajax call
   $.ajax({
     url: queryURL,
@@ -14,7 +14,7 @@ $("#search").on("click", function (event) {
     console.log(response);
     $("#city-name").text("Today: " + response.name)
     $("#city-weather").text("Weather: " + response.weather[0].main)
-    $("#city-temp").text("Temperature: " + (response.main.temp) + " degrees celsius")
+    $("#city-temp").text("Temperature: " + converter(response.main.temp) + " ℃")
     $("#city-humidity").text("Humidity: " + response.main.humidity + "%")
     $("#city-wind").text("Wind Speed: " + response.wind.speed + "MPH")
 
@@ -52,11 +52,11 @@ $("#search").on("click", function (event) {
       $(".5Day5-weather").text("Weather: " + fivedays.list[39].weather[0].main)
 
       //5 day forecast temperature
-      $(".5Day1-temp").text("Temperature: " + fivedays.list[10].main.temp + " degrees celsius")
-      $(".5Day2-temp").text("Temperature: " + fivedays.list[18].main.temp + " degrees celsius")
-      $(".5Day3-temp").text("Temperature: " + fivedays.list[26].main.temp + " degrees celsius")
-      $(".5Day4-temp").text("Temperature: " + fivedays.list[34].main.temp + " degrees celsius")
-      $(".5Day5-temp").text("Temperature: " + fivedays.list[39].main.temp + " degrees celsius")
+      $(".5Day1-temp").text("Temperature: " + converter(fivedays.list[10].main.temp) + " ℃")
+      $(".5Day2-temp").text("Temperature: " + converter(fivedays.list[18].main.temp) + " ℃")
+      $(".5Day3-temp").text("Temperature: " + converter(fivedays.list[26].main.temp) + " ℃")
+      $(".5Day4-temp").text("Temperature: " + converter(fivedays.list[34].main.temp) + " ℃")
+      $(".5Day5-temp").text("Temperature: " + converter(fivedays.list[39].main.temp) + " ℃")
 
       //5 day forecast humidity
       $(".5Day1-humidity").text("Humidity: " + fivedays.list[10].main.humidity + " %")
@@ -65,26 +65,54 @@ $("#search").on("click", function (event) {
       $(".5Day4-humidity").text("Humidity: " + fivedays.list[34].main.humidity + " %")
       $(".5Day5-humidity").text("Humidity: " + fivedays.list[39].main.humidity + " %")
 
-     
+
     });
 
   });
+saveHistory(citySearch)
+displayHistory()
+});
 
-  /*
+function converter (kelv2deg) {
+return Math.floor (kelv2deg - 273.15) 
+}
+
+ /*
   //UV warnings
     if (UVresponse > 3){
       ("#city-uv").addClass("uvLevel")
   }
   */
 
+
+
+
   //Save searches to local storage
-  localStorage.setItem("searched", citySearch)
-  /*
-  //retrieve searches from local storage
-  var pastSearch = localStorage.getItem(citySearch)
-  $("#previous-searches").innerHTML =  localStorage.getItem (pastSearch)//doesnt work!!!
-  */
-});
+  var pastSearches = []
+  function saveHistory(newCity){
+    pastSearches.push(newCity) 
+    localStorage.setItem("searched", JSON.stringify(pastSearches))
+  }
+
+  function readHistory(){
+    var storedString = localStorage.getItem("searched")
+    
+    if (storedString) {
+      var storedArray = JSON.parse(storedString)
+      pastSearches = storedArray
+    }
+  }
+
+  function displayHistory(){
+    $("#previous-searches").empty()
+    for (let i = 0; i < pastSearches.length; i++) {
+      var pastSearch = pastSearches [i];
+      $("#previous-searches").append( pastSearch + ", ")
+    }
+  }
+
+  readHistory()
+  displayHistory()
 
 //button to clear local storage
 $('.clearSearches').on('click', clearLocalStorage);
