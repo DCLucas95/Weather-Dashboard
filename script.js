37cc8c9a14f13dbb43d5e305ab660837faea61f8
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-20 11:06:04 +1000

    added reads for city, temp, weather and wind speed. need to convert

--- script.js
+++ script.js
@@ -15,5 +15,35 @@
     method: "GET"
   }).then(function(response) {
     console.log(response);
+    $("#city-name").text(response.name)
+    $("#city-weather").text("Weather: "+ response.weather[0].main)
+    $("#city-temp").text("Temperature: "+response.main.temp +" degrees celsius")
+    $("#city-wind").text("Wind Speed: "+ response.wind.speed)
+    $("#city-uv").text("City: "+ response.name)
   });
 });
+
+function kelvinConverter(K) {
+  return Math.floor((K - 273.15) *1.8 +32);
+}
+
+
+
+
+
+
+/*
+ // Creating a paragraph tag with the selected city
+ var city = $("#citySearched").text(response.);
+
+ // Creating an image tag
+ var personImage = $("<img>");
+
+ // Giving the image tag an src attribute of a proprty pulled off the
+ // result item
+ personImage.attr("src", results[i].images.fixed_height.url);
+
+ // Appending the paragraph and personImage we created to the "gifDiv" div we created
+ gifDiv.append(p);
+ gifDiv.append(personImage);
+ */
