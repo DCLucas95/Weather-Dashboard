7cd2ff9b8a772709e5b1d36c4db7b49d612828e0
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-20 10:21:22 +1000

    fixed search error, needed to move variables into function

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+//Start process when search button clicked
+  $("#search").on("click", function(event) {
+    
   //variables
     var citySearch = $("#citySearched").val()
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearch + "&appid=b97ce200929c2749eca4924f16dc7e98";
-
-//Start process when search button clicked
-  $("#search").on("click", function(event) {
     event.preventDefault();
 
     console.log(citySearch)
